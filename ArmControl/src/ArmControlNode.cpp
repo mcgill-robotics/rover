e9f622ea001a441f246f904c5321bf165934c333
Hongjun Chen <nanjingchj@gmail.com>
2020-10-12 21:42:11 +0000

    Updated arm control project setup

--- ArmControl/src/ArmControlNode.cpp
+++ ArmControl/src/ArmControlNode.cpp
@@ -1,12 +1,16 @@
 #include <ros/ros.h>
 #include <std_msgs/String.h>
 
+
 int main(int argc, char** argv)
 {
     ros::init(argc, argv, "ArmControl");
     ros::NodeHandle nodeHandle;
     ros::Rate loopRate(60);
     
+    int* ptr = new int[10];
+    ptr[10] = 20;
+
     while (ros::ok())
     {
     }

d9fabe0053d87c9971956d730dbab712e1495854
Hongjun Chen <nanjingchj@gmail.com>
2020-10-12 13:22:04 +0000

    Arm control project setup

--- ArmControl/src/ArmControlNode.cpp
+++ ArmControl/src/ArmControlNode.cpp
@@ -3,6 +3,13 @@
 
 int main(int argc, char** argv)
 {
+    ros::init(argc, argv, "ArmControl");
+    ros::NodeHandle nodeHandle;
+    ros::Rate loopRate(60);
+    
+    while (ros::ok())
+    {
+    }
 
     return 0;
 }

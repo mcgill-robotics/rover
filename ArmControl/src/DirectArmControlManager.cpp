fb84469be46f52efe109a12c87f7fbf8b68ac381
Hongjun Chen <nanjingchj@gmail.com>
2020-10-20 14:15:30 +0000

    Direct arm control abstraction layer

--- ArmControl/src/DirectArmControlManager.cpp
+++ ArmControl/src/DirectArmControlManager.cpp
@@ -3,14 +3,25 @@
 
 namespace Rover 
 {
+    using namespace ArmControl;
+
     DirectArmControlManager::DirectArmControlManager(ros::NodeHandle& handle)
-        : m_ArmData(), m_NodeHandle(handle), m_ArmMotorPublisher(handle.advertise<ArmControl::ArmMotorData>("arm_control_data", 30))
-    {}
+        : m_ArmData(), m_NodeHandle(handle), m_ArmMotorPublisher(handle.advertise<ArmControl::ArmMotorData>("arm_control_data", 16)),
+          m_ActiveMotor(0), m_LatestTimestep(0.0f)
+    {
+        boost::function<void(const ProcessedControllerInput& input)> bindedFunc = [this](const ProcessedControllerInput& input) -> void
+        {
+            DirectControllCallback(input);
+        };
+        m_ProcessedInputSubscriber = handle.subscribe<ProcessedControllerInput>("ProcessedArmControllerInput", 16, bindedFunc);
+    }
 
     void DirectArmControlManager::Update(float timestep)
     {
         ROS_INFO("Elapsed: %f", timestep);
+        m_LatestTimestep = timestep;
         m_ArmMotorPublisher.publish(m_ArmData);
+        ros::spinOnce(); // if applicable, call controller input callback
     }
 }
 
--- ArmControl/src/DirectArmControlManager.h
+++ ArmControl/src/DirectArmControlManager.h
@@ -1,6 +1,7 @@
 #pragma once
 #include <stdlib.h>
 #include "ArmControl/ArmMotorData.h"
+#include "ArmControl/ProcessedControllerInput.h"
 #include <ros/ros.h>
 
 namespace Rover
@@ -27,11 +28,116 @@ namespace Rover
         DirectArmControlManager(ros::NodeHandle& handle);
 
         void Update(float timestep);
+
+        float& GetMotorData(size_t index)
+        {
+            ROS_ASSERT(index <= 4 && index >= 0);
+            // TODO : optimize
+            switch (index)
+            {
+                case 0:
+                {
+                    return m_ArmData.Axis0;
+                }
+                case 1:
+                {
+                    return m_ArmData.Axis1;
+                }
+                case 2:
+                {
+                    return m_ArmData.Axis2;
+                }
+                case 3:
+                {
+                    return m_ArmData.Axis3;
+                }
+                case 4:
+                {
+                    return m_ArmData.Axis4;
+                }
+                default:
+                {
+                    ROS_ASSERT(false);
+                }
+            }
+        }
+
+        float& operator[](size_t index)
+        {
+            return GetMotorData(index);
+        }
+
+        void SelectMotor(size_t index)
+        {
+            ROS_ASSERT(index <= 4 && index >= 0);
+            m_ActiveMotor = index;
+        }
+
+        void DisconnectDirectInput()
+        {
+            m_ProcessedInputSubscriber.shutdown();
+        }
+
+        void ConnectDirectInput()
+        {
+            boost::function<void(const ProcessedControllerInput& input)> bindedFunc = [this](const ProcessedControllerInput& input) -> void
+            {
+                DirectControllCallback(input);
+            };
+            m_ProcessedInputSubscriber = m_NodeHandle.subscribe<ProcessedControllerInput>("ProcessedArmControllerInput", 16, bindedFunc);
+        }
         
     private:
+        void DirectControllCallback(const ProcessedControllerInput& input)
+        {
+            ROS_INFO("Pitch: %f\nRoll: %f\nYall: %f\nBtnCycleForward: %d\nBtnCycleBackward: %d", 
+                input.PitchAxis, 
+                input.RollAxis, // ignore
+                input.YawAxis, // ignore
+                input.BtnCycleForward, 
+                input.BtnCycleForward
+            );
+            if (input.BtnCycleForward)
+            {
+                CycleForward();
+            }
+            if (input.BtnCycleBackward)
+            {
+                CycleBackward();
+            }
+            GetMotorData(m_ActiveMotor) += input.PitchAxis * m_LatestTimestep;
+        }
+
+        void EnsureMotorIndexInRange()
+        {
+            if (m_ActiveMotor > 4)
+            {
+                m_ActiveMotor -= 5;
+            }
+            if (m_ActiveMotor < 0)
+            {
+                m_ActiveMotor += 5;
+            }
+        }
+
+        void CycleForward()
+        {
+            m_ActiveMotor++;
+            EnsureMotorIndexInRange();
+        }
+
+        void CycleBackward()
+        {
+            m_ActiveMotor--;
+            EnsureMotorIndexInRange();
+        }
+
         ArmMotorData m_ArmData;
         ros::NodeHandle& m_NodeHandle;
         ros::Publisher m_ArmMotorPublisher;
+        ros::Subscriber m_ProcessedInputSubscriber;
+        size_t m_ActiveMotor;
+        float m_LatestTimestep;
     };
 }
 

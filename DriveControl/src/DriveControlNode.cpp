9a177000a0f69bc8b039b6d836b63e9b7db0eda0
JJ0704 <jiajunliu07@gmail.com>
2020-10-30 22:51:37 -0400

    added some modifications

--- DriveControl/src/DriveControlNode.cpp
+++ DriveControl/src/DriveControlNode.cpp
@@ -1,6 +1,6 @@
 #include <ros/ros.h>
-#include <std_msgs/String.h>
 #include <sensor_msgs/Joy.h>
+#include <DriveControl/WheelSpeed.h>
 
 
 class Drive_Control
@@ -15,26 +15,46 @@ private:
 
     ros::NodeHandle handle;
 
-    //ros::Publisher drive_pub;
-    ros::Subscriber drive_sub;
+    ros::Publisher vel_pub;
+    ros::Subscriber joy_sub;
 
-    int linear;
-    int angular;
+    int linear_axis;
+    int rotation_axis;
+    double linear_vel;
+    double rotation_vel;
 
 };
 
 
 Drive_Control::Drive_Control():
-    linear(1),
-    angular(2)
+    //default parametes in case the parameters cannot could not be retrieved
+    linear_axis{1}, 
+    rotation_axis{0}
 {
 
-    drive_sub =  handle.subscribe<sensor_msgs::Joy>("joy", 10, &Drive_Control::joyCallback, this);
+    handle.param("linear_axis", linear_axis, linear_axis);
+    handle.param("rotation_axis", rotation_axis, rotation_axis);
 
+    handle.setParam("linear_axis",1);
+    handle.setParam("rotation_axis", 0);
+    
+    vel_pub = handle.advertise<DriveControl::WheelSpeed>("WheelSpeed", 1);
+
+    joy_sub =  handle.subscribe<sensor_msgs::Joy>("joy", 10, &Drive_Control::joyCallback, this);
+
+    
 }
 
 void Drive_Control::joyCallback(const sensor_msgs::Joy::ConstPtr& joy)
 {
+    DriveControl::WheelSpeed speed;
+    
+    //speed.Wheel_Speed[0] = joy->axes[linear_axis];
+    //speed.Wheel_Speed[1] = joy->axes[rotation_axis];
+
+    vel_pub.publish(speed);
+
+
 
 }
 

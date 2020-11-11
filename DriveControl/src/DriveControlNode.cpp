2fe3ff2aedf997574445db1e9128459c48902b6e
JJ0704 <jiajunliu07@gmail.com>
2020-11-10 21:05:46 -0500

    set up the math equation

--- DriveControl/src/DriveControlNode.cpp
+++ DriveControl/src/DriveControlNode.cpp
@@ -18,25 +18,35 @@ private:
     ros::Publisher vel_pub;
     ros::Subscriber joy_sub;
 
-    int linear_axis;
-    int rotation_axis;
-    double linear_vel;
-    double rotation_vel;
+    static constexpr int linear_axis = 1; //1 is the index controlling up/down axis on xbox 360 controller
+    static constexpr int rotation_axis = 0; //0 is the index controlling up/down axis on xbox 360 controller
+    
+    static constexpr float linear_scale = 0.49;
+    static constexpr float rotation_scale = 0.49;
+
+    static constexpr float radius_wheel = 0.15; //radius of the wheel in meters
+    static constexpr float length = 0.4; //wheel to center in meters
+
+    float linear_veloctiy; //V / linear speed of rover
+    float rotational_velocity; //omega / turning speed of the whole rover around axis at its location
+
+    static constexpr float maxV = 2.42; //maximum linear velocity in m/s
+    static constexpr float maxOmega = 6.048; //maximum omega in rad/s
+
 
 };
 
 
-Drive_Control::Drive_Control():
-    //default parametes in case the parameters cannot could not be retrieved
-    linear_axis{1}, 
-    rotation_axis{0}
+Drive_Control::Drive_Control()
+   
 {
 
-    handle.param("linear_axis", linear_axis, linear_axis);
-    handle.param("rotation_axis", rotation_axis, rotation_axis);
 
-    handle.setParam("linear_axis",1);
-    handle.setParam("rotation_axis", 0);
+    //handle.setParam("linear_scale", 0.5);
+    //handle.setParam("rotation_scale",0.5);
+
+
+
     
     vel_pub = handle.advertise<DriveControl::WheelSpeed>("WheelSpeed", 1);
 
@@ -48,11 +58,17 @@ Drive_Control::Drive_Control():
 void Drive_Control::joyCallback(const sensor_msgs::Joy::ConstPtr& joy)
 {
     DriveControl::WheelSpeed speed;
-    
-    //speed.Wheel_Speed[0] = joy->axes[linear_axis];
-    //speed.Wheel_Speed[1] = joy->axes[rotation_axis];
 
-    vel_pub.publish(speed);
+    
+    linear_veloctiy = joy->axes[linear_axis] * maxV * linear_scale; //linear velocity received from controller input
+    rotational_velocity = joy->axes[rotation_axis] * maxOmega * rotation_scale; //rotational velocity received from controller input
+
+    speed.Wheel_Speed[0] = (linear_veloctiy + (rotational_velocity*length))/radius_wheel; //left wheel speed
+    speed.Wheel_Speed[1] = (linear_veloctiy - (rotational_velocity*length))/radius_wheel; //right wheel speed
+
+
+
+    vel_pub.publish(speed);  //publish wheelspeed message to WheelSpeed topic
 
 
 
@@ -65,7 +81,7 @@ void Drive_Control::joyCallback(const sensor_msgs::Joy::ConstPtr& joy)
 int main(int argc, char** argv)
 {
     ros::init(argc,argv, "DriveControlNode");
-    Drive_Control drive;
+    Drive_Control rover_drive;
 
     ros::spin();
 

1a02fc42a2fb94cdacb48a9914d9483970b11006
JJ0704 <jiajunliu07@gmail.com>
2020-11-22 12:06:27 -0500

    fixed math equation

--- DriveControl/src/drive_control_node.cpp
+++ DriveControl/src/drive_control_node.cpp
@@ -21,8 +21,8 @@ private:
     static constexpr int linearAxis = 1; //1 is the index controlling up/down axis on xbox 360 controller
     static constexpr int rotationAxis = 0; //0 is the index controlling up/down axis on xbox 360 controller
     
-    static constexpr float linearScale = 0.49;
-    static constexpr float rotationScale = 0.49;
+    static constexpr float kv = 0.49; //just a constant
+    static constexpr float ko = 0.49; //just a constant
 
     static constexpr float radiusWheel = 0.15; //radius of the wheel in meters
     static constexpr float length = 0.4; //wheel to center in meters
@@ -30,10 +30,12 @@ private:
     float linearVelocity; //V / linear speed of rover
     float rotationalVelocity; //omega / turning speed of the whole rover around axis at its location
 
-    static constexpr float maxV = 2.42; //maximum linear velocity in m/s
-    static constexpr float maxOmega = 6.048; //maximum omega in rad/s
+    static constexpr float maxV = 2.41; //maximum linear velocity in m/s
+    static constexpr float maxOmega = 6.047; //maximum omega in rad/s
 
-
+    double x; //hold value of joystick horizontally
+    double y; //hold value of joystick vertically
+    
 };
 
 
@@ -41,6 +43,7 @@ drive_control::drive_control()
    
 {
 
+    handle.setParam("dev", "/dev/input/js0"); //set the joystick device from which to receive joy messages
 
     velPub = handle.advertise<DriveControl::WheelSpeed>("wheel_speed", 1);
 
@@ -53,9 +56,11 @@ void drive_control::joy_callback(const sensor_msgs::Joy::ConstPtr& joy)
 {
     DriveControl::WheelSpeed speed;
 
+    x = joy->axes[rotationAxis];
+    y = joy->axes[linearAxis];
     
-    linearVelocity = joy->axes[linearAxis] * maxV * linearScale; //linear velocity received from controller input
-    rotationalVelocity = joy->axes[rotationAxis] * maxOmega * rotationScale; //rotational velocity received from controller input
+    linearVelocity = kv*0.5* maxV * y * (2-abs(x)); //linear velocity received from controller input
+    rotationalVelocity = ko * 0.5 * maxOmega * x * (2-abs(y)); //rotational velocity received from controller input
 
     speed.wheel_speed[0] = (linearVelocity + (rotationalVelocity*length))/radiusWheel; //left wheel speed
     speed.wheel_speed[1] = (linearVelocity - (rotationalVelocity*length))/radiusWheel; //right wheel speed

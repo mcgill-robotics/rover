8348cfaf64003cdb0b22783a32019dcbdccc813e
Hongjun Chen <nanjingchj@gmail.com>
2021-05-15 12:47:49 -0400

    main.py setup

--- ui/src/main.py
+++ ui/src/main.py
@@ -1,6 +1,3 @@
-#!/usr/bin/python3
-
-
 import sys
 import rospy as rp 
 from std_msgs import *
@@ -9,15 +6,25 @@ import asyncio
 import gui.main_window
 
 
+ui: gui.main_window.Ui_MainWindow = None
+pub: rp.Publisher = None
+
+
 def pbClicked():
-    rp.loginfo("Hello")
+    pub.publish("test")
 
 
-def main():
+def onTestReceived(data: msg.String):
+    data: str = str(data)
+
+
+async def main():
     rp.init_node("ui")
     app = QtWidgets.QApplication(sys.argv)
     wnd = QtWidgets.QMainWindow()
     ui = gui.main_window.Ui_MainWindow()
+    sub = rp.Subscriber("test", msg.String, onTestReceived)
+    pub = rp.Publisher("test2", msg.String)
     ui.setupUi(wnd)
     pb: QtWidgets.QPushButton = ui.pushButton
     pb.clicked.connect(pbClicked)
@@ -26,4 +33,4 @@ def main():
 
 
 if __name__ == "__main__":
-    main()
+    asyncio.run(main())

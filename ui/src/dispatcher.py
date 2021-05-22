6398b8ee63f739f36416198bc12c5dc0a4e413da
Hongjun Chen <nanjingchj@gmail.com>
2021-05-22 14:12:09 -0400

    UI fixes

--- ui/src/dispatcher.py
+++ ui/src/dispatcher.py
@@ -1,5 +1,13 @@
+import sys
+import rospy as rp 
+from std_msgs import *
+from PyQt5 import QtCore, QtGui, QtWidgets, uic
+import asyncio
+from functools import partial
+
+
 class Dispatcher:
     def __init__(self):
         pass
-    
+
 

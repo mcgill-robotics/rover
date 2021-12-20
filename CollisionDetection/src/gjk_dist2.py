82064da7e8cd412a89147eecad538a1ac64cd4b8
Parth Gupta <parthgupta023@gmail.com>
2021-12-20 13:52:42 -0500

    Added distance feature in gjk_dist2.py

---
+++ CollisionDetection/src/gjk_dist2.py
@@ -0,0 +1,143 @@
+import numpy as np
+
+def findFurthest(polygon, direction):
+    bestPoint = polygon[0]
+    bestDot = np.dot(bestPoint, direction)
+
+    for i in range(1, len(polygon)):
+        p = polygon[i]
+        distance = np.dot(p, direction)
+
+        if distance > bestDot:
+            bestDot = distance
+            bestPoint = p
+
+    return bestPoint
+
+def support(poly1, poly2, direction):
+    return np.subtract(findFurthest(poly1, direction), findFurthest(poly2, np.negative(direction)))
+
+
+def collide(shape1, shape2):
+    s = support(shape1, shape2, (1, 0, 0))
+    simplex = [s]
+    d = list(np.negative(s))
+
+    while (True) :
+        a = support(shape1, shape2, d)
+
+        if(np.dot(a, d) <= 0):
+            return [False, d] # no collision
+
+        simplex.append(a)
+
+        if doSimplex(simplex, d)[0]:
+            return [True, 0] 
+
+def sameDirection(v1, v2):
+    return np.dot(v1, v2) > 0
+
+def projection(v1, v2):
+    '''Gives projection of v1 onto v2'''
+    m = 0           
+    for val in v2:
+        m += val*val
+
+    return np.multiply(np.divide(np.dot(v2, v1), m), v2)
+
+def doSimplexLine(simplex, d):
+    a = simplex[0]
+    b = simplex[1]
+    a0 = np.negative(a)
+    ab = np.subtract(b, a)
+    
+    if sameDirection(ab, a0) : 
+
+        # Triple product doesn't give info about distance, so we use vector rejection for the line case
+        proj = projection(a0, ab)
+        d = np.subtract(a0, proj)       # Closest point to O
+
+    else:
+        simplex = [a] # remove b from simplex
+        d = a0
+
+    return d   
+
+def doSimplexTriangle(simplex, d):
+    a = simplex[0]
+    b = simplex[1]
+    c = simplex[2]
+    a0 = np.negative(a)
+    ab = np.subtract(b, a)
+    ac = np.subtract(c, a)
+    abc = np.cross(ab, ac)
+
+    # REGION 1
+    if sameDirection(np.cross(abc, ac), a0) and sameDirection(ac, a0):
+        return doSimplexLine([a, c], d)
+
+    # REGION 4
+    elif sameDirection(np.cross(abc, ac), a0) and sameDirection(ab, a0):
+        return doSimplexLine([a, b], d)
+    elif sameDirection(np.cross(ab, abc), a0) and sameDirection(ab, a0):
+        return doSimplexLine([a, b], d)
+    
+    # REGIONS 2 and 3
+    elif not sameDirection(np.cross(abc, ac), a0) or not sameDirection(np.cross(ab, abc), a0):
+        d = projection(a0, abc)
+        return d
+
+
+
+
+def doSimplexTetrahedron(simplex, d):
+    a = simplex[0]
+    b = simplex[1]
+    c = simplex[2]
+    d = simplex[3]
+    a0 = np.negative(a)
+    ab = np.subtract(b, a)
+    ac = np.subtract(c, a)
+    ad = np.subtract(d, a)
+
+    abc = np.cross(ab , ac)
+    acd = np.cross(ac, ad)
+    adb = np.cross(ad, ab)
+
+    #if abc is the same direction as a0 then
+    if sameDirection(abc, a0):
+        newSimplex = [ a,b,c]
+        return doSimplexTriangle(newSimplex, d)
+
+    #if acd is the same direction as a0 then
+    if sameDirection(acd, a0):
+        newSimplex = [ a, c, d]
+        return doSimplexTriangle(newSimplex, d)
+
+    #if adb is the same direction as a0 then
+    if sameDirection(adb, a0):
+        newSimplex = [ a,d,b]
+        return doSimplexTriangle(newSimplex, d)
+    
+    return d
+
+def doSimplex(simplex, d):
+    l = len(simplex)
+
+    #LINE CASE -- 2 POINTS IN THE SIMPLEX
+    if l == 2:
+        doSimplexLine(simplex, d)
+    
+    #TRIANGLE CASE -- 3 POINTS IN THE SIMPLEX
+    elif l == 3:
+        doSimplexTriangle(simplex, d)
+    
+    #TETRAHEDRON CASE -- 4 POINTS IN THE SIMPLEX
+    else:
+        doSimplexTetrahedron(simplex, d)
+
+    if d == 0:
+        return [True, 0]
+    else:
+        return [False, d]
+   
--- CollisionDetection/test/basicTests.py
+++ CollisionDetection/test/basicTests.py
@@ -2,7 +2,7 @@
 
 import sys
 sys.path.insert(0, '..\src')
-import gjk_boolean as gjk
+import gjk_dist2 as gjk
 
 #the shapes below are all corner points from cubes or rectangular prisms
 shape1 = [(0,0,1), (0,1,1), (1,1,1), (1,0,1), (0,0,0), (1,0,0), (0,1,0), (1,1,0)]
@@ -12,7 +12,12 @@ shape4 = [(0,0,6), (0,1,6), (1,1,6), (1,0,6), (0,0,5), (1,0,5), (0,1,5), (1,1,5)
 shape5 = [(0,0,2), (0,1,2), (1,1,2), (1,0,2), (0,0,0), (1,0,0), (0,1,0), (1,1,0)]
 shape6 = [(0,0,6), (0,1,6), (1,1,6), (1,0,6), (0,0,1), (1,0,1), (0,1,1), (1,1,1)]
 
+shape7 = [(3, 1, 1), (2, 5, -3), (0, 0, 0)]
+shape8 = [(3, 3, -3), (1, 2, -2), (1, 3, 1)]
+
 print(gjk.collide(shape1, shape2)) #should intersect
 print(gjk.collide(shape1, shape3)) #should not intersect
 print(gjk.collide(shape1, shape4)) #should not intersect
 print(gjk.collide(shape5, shape6)) #should intersect
+
+print(gjk.collide(shape7, shape8)) # Should intersect

0b279c866c40ddef638afe2242367886e3dba206
Parth Gupta <parthgupta023@gmail.com>
2022-01-04 17:30:31 +0000

    Added comments to main.py

--- UI_App/src/main.py
+++ UI_App/src/main.py
@@ -1,3 +1,4 @@
+# Imports 
 from ui_layout import Ui_MainWindow
 
 
@@ -6,13 +7,28 @@ from PyQt5 import QtCore as qtc
 
 
 class UI(qtw.QMainWindow, Ui_MainWindow):
+    '''
+    Main application interface. It inherits from Ui_MainWindow which is the base layout for the
+    app present in ui_layout.py. Most of the app is controlled from this class.
+    '''
+
     def __init__(self, *args, **kwargs):
+        # Setup the UI from Ui_MainWindow
         super().__init__(*args, **kwargs)
-        self.setupUi(self)
+        self.setupUi(self)     
+
+        # Listeners
         self.control_selector.currentTextChanged.connect(self.on_control_changed)
 
 
     def on_control_changed(self, value):
+        '''
+        Method takes in the UI and the value of the control_selector combo box. It gets 
+        called whenever the ComboBox value gets changed. 
+        #TODO: Waiting for system controls to be implemented so that this selector can 
+        select the control system.
+        '''
+        
         if value == "Arm-Cartesian Control":
             pass
             # return arm file

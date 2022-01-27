8b8fb4a6db19f1659fc832e5bcfdca1d37d4b95b
Shaswata79 <shaswatabhatt2030@gmail.com>
2022-01-27 18:52:16 -0500

    Initialized drive node

--- drive_control/src/drive_control_node.py
+++ drive_control/src/drive_control_node.py
@@ -1,1 +1,23 @@
 import rospy
+import Steering
+from std_msgs.msg import String
+
+class Node_DriveControl():
+
+    def __init__(self):
+        self.wheel_radius = 10
+
+        self.angular_velocity = 
+
+        pub = rospy.Publisher('angular_velocity', String, queue_size=10)
+        rospy.init_node('angular_velocity_publisher')
+        rate = rospy.Rate(10)
+
+    def publisher():
+        
+
+
+
+if __name__ == "__main__":
+    driver = Node_DriveControl()
+    rospy.spin()

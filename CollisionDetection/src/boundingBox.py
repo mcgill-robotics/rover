7f621f76caa69715967ad918e3e59bce5e748371
BuildTools <unconfigured@null.spigotmc.org>
2022-02-17 17:43:48 -0500

    Back to 1 BB file

--- CollisionDetection/src/boundingBox.py
+++ CollisionDetection/src/boundingBox.py
@@ -18,12 +18,30 @@ def generateBB(points):  # Generates a bounding box for an nx3 np.array
                          [maxs[0], maxs[1], mins[2]],
                          [maxs[0], mins[1], maxs[2]],
                          [maxs[0], maxs[1], maxs[2]]])
-    return vertices  # return an 8x3 np.array which are the bouding box vertices
+    return vertices  # return an 8x3 np.array which are the bounding box vertices
+
+
+def generateOrientedBB(points):  # Generates an oriented bounding box for an nx3 np.array
+    V = np.linalg.svd(points)[2]
+    B = np.dot(points, V)
+
+    mins = np.min(B, axis=0)
+    maxs = np.max(B, axis=0)
+    vertices = np.array([[mins[0], maxs[1], maxs[2]],
+                         [mins[0], mins[1], maxs[2]],
+                         [mins[0], maxs[1], mins[2]],
+                         [mins[0], mins[1], mins[2]],
+                         [maxs[0], mins[1], mins[2]],
+                         [maxs[0], maxs[1], mins[2]],
+                         [maxs[0], mins[1], maxs[2]],
+                         [maxs[0], maxs[1], maxs[2]]])
+
+    return np.dot(vertices, V.transpose())  # return an 8x3 np.array which are the oriented bounding box vertices
 
 
 if __name__ == '__main__':  # Plots the points and their bounding box
-    pts = pointGenerator(1000)
-    vertices = generateBB(pts)
+    pts = pointGenerator(100)
+    vertices = generateOrientedBB(pts)
 
     fig = plt.figure()
     ax = fig.add_subplot(projection='3d')

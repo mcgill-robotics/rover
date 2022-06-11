49979f5044a05e0993b35188602d888e5e8385b3
Walid_Aissa <temportime123@gmail.com>
2022-06-11 18:56:20 -0400

    Updated the Arm UI Backend

--- UI_App/src/main.py
+++ UI_App/src/main.py
@@ -8,9 +8,11 @@ sys.path.append(currentdir)
 from ui_layout import Ui_MainWindow
 import rospy
 from drive import Drive_Backend
+from arm_backend import Arm_Backend
 from UI_App.msg import WheelSpeed
 from geometry_msgs.msg import Twist
 from visualization_msgs.msg import MarkerArray
+from arm_control.msg import ArmStatusFeedback
 
 from PyQt5 import QtWidgets as qtw
 from PyQt5 import QtCore as qtc
@@ -35,10 +37,12 @@ class UI(qtw.QMainWindow, Ui_MainWindow):
 
         #drive setup
         self.drive_backend = Drive_Backend(self.Drive)
+        self.arm_backend = Arm_Backend(self.Arm)
         self.drive_wheel_velocity_subscriber = rospy.Subscriber('/wheel_velocity_cmd', WheelSpeed, self.drive_backend.update_wheel_velocities)
         self.drive_twist_subscriber = rospy.Subscriber("rover_velocity_controller/cmd_vel", Twist, self.drive_backend.update_twist_data)
-        self.drive_location_subscriber = rospy.Subscriber('/visualization_marker_array', MarkerArray, self.drive_backend.update_robot_location) 
-
+        self.drive_location_subscriber = rospy.Subscriber('/visualization_marker_array', MarkerArray, self.drive_backend.update_robot_location)
+        self.arm_hand_subscriber= rospy.Subscriber("arm_state_data", ArmStatusFeedback, self.arm_backend.update_joints) 
+    
     
     
     def arm_error_toggle(self, signal):

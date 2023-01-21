56375bf5a118ebee03c41bda2c4f905e19e5f4aa
Mingyue <mingyue.deng@mail.mcgill.ca>
2023-01-21 15:16:02 -0500

    import meshes

--- drive_control/test/pub.py
+++ drive_control/test/pub.py
@@ -6,16 +6,16 @@ from drive_control.msg import WheelSpeed
 
 def pub():
     robot_twist = Twist()
-    robot_twist.angular.z = 30
-    robot_twist.linear.x = 0.5
+    robot_twist.angular.z = 0
+    robot_twist.linear.x = 1
     # robot_twist.angular.z = 1500
     twist_pub = rospy.Publisher("rover_velocity_controller/cmd_vel", Twist, queue_size=10)
     
     feedback = WheelSpeed()
-    feedback.left[0] = 0
-    feedback.left[1] = 0
-    feedback.right[0] = 0
-    feedback.right[1] = 0
+    feedback.left[0] = 1
+    feedback.left[1] = 1
+    feedback.right[0] = 1
+    feedback.right[1] = 1
     feedback_pub = rospy.Publisher('/feedback_velocity', WheelSpeed, queue_size=10)
 
     rospy.init_node('test_pub', anonymous=True)
@@ -24,6 +24,7 @@ def pub():
         robot_twist.linear.x = 30 + i
         twist_pub.publish(robot_twist)
         feedback_pub.publish(feedback)
+        rospy.loginfo(feedback)
         time.sleep(0.5)
         i += 10
         

5dd7c85358a3fb5efeb92263a0b36c8a0ded1488
amotaouakkil123 <adam.motaouakkil@mail.mcgill.ca>
2023-06-06 19:41:56 -0400

    Added motor speeds conversions, fix sim topics.

--- drive_control/src/drive_control_node.py
+++ drive_control/src/drive_control_node.py
@@ -13,6 +13,13 @@ import scipy.stats as st
 
 class Node_DriveControl():
 
+    MAX_NO_STEER = 21.333
+    MAX_WITH_STEER = 95.99
+    MIN_NO_STEER = -MAX_NO_STEER
+    MIN_WITH_STEER_1 = -53.33
+    MIN_WITH_STEER_2 = -MAX_WITH_STEER
+
+
     def __init__(self):
         """
         Hard-coded the simulation rover measurement, will need to adapt it towards the real model.
@@ -89,10 +96,43 @@ class Node_DriveControl():
             cmd.left[0], cmd.left[1] = correct_lf, correct_lb
             cmd.right[0], cmd.right[1] = correct_rf, correct_rb
 
+            motor_lf = self.motor_speed(correct_lf)
+            motor_lb = self.motor_speed(correct_lb)
+            motor_rf = self.motor_speed(correct_rf)
+            motor_rb = self.motor_speed(correct_rb)
+
+            
+            print(f"Motor values: {[motor_lf, motor_lb, motor_rf, motor_rb]}")
+
             self.angular_velocity_publisher.publish(cmd)
 
             self.rate.sleep()
             print(cmd)
+        
+    # def motor_speed(self, cur_val):
+                
+    # TODO: CHECK MOTOR VALUES AND VERIFY CALCULATIONS.
+    def motor_speed(self, cur_val):
+        motor_val = cur_val / Node_DriveControl.MAX_NO_STEER
+        if motor_val < 1 and motor_val > 0:
+            return motor_val
+
+        if motor_val > 1:
+            motor_val = cur_val / Node_DriveControl.MAX_WITH_STEER
+            return motor_val
+        
+        if motor_val < 0:
+            motor_val = -(cur_val / Node_DriveControl.MIN_NO_STEER)
+            if motor_val < -1:
+                motor_val = -(cur_val / Node_DriveControl.MIN_WITH_STEER_1)
+                if motor_val < -1:
+                    motor_val = -(cur_val / Node_DriveControl.MIN_WITH_STEER_2)
+            return motor_val
+        
+        return motor_val
+
+
+        
 
 if __name__ == "__main__":
     driver = Node_DriveControl()

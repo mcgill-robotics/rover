eb9ef82b80f4cd31fd7fdea40f6928b4d4c433be
Joseph Saliba <josephsaliba54@gmail.com>
2023-08-12 01:55:53 -0400

    Realsense angle calculation + arrow on gps

--- ui_app/src/gps_backend.py
+++ ui_app/src/gps_backend.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 # Imports
+import numpy as np
 import os, sys
 currentdir = os.path.dirname(os.path.realpath(__file__))
 sys.path.append(currentdir)
@@ -10,16 +11,20 @@ class GPS_Backend():
         self.ui = gps_tab
         self.control_station = [45.5056037902832, -73.57576751708984]
         self.fixed_points = [
-            [45.50570297241211, -73.57591247558594], 
+            [45.50570297241211, -73.57591247558594],
             [45.50539779663086, -73.5755615234375],
             [45.50568771362305, -73.5759506225586],
             [45.50548771362305, -73.5756506225586],
         ]
 
-        self.robot_marker = None
+        self.robot_marmathker = None
+        self.first_gps_data = []
         
 
     def plot_gps_figure(self,gps_data):
+        self.first_gps_data.append([gps_data.data[0], gps_data.data[1]])
+        
+
         self.ui.gps_data.setText("latitude: " + str(gps_data.data[0])+ "\nlongitude: " + str(gps_data.data[1]))
 
         self.ui.gps_fig.clear()
@@ -30,6 +35,15 @@ class GPS_Backend():
         lats, lons = zip(*self.fixed_points)
 
         ax.scatter(lons, lats, c='red', label='Goal')
+
+        if len(self.first_gps_data) == 2:
+            dx = self.first_gps_data[1][0] - self.first_gps_data[0][0]
+            dy = self.first_gps_data[1][1] - self.first_gps_data[0][1]
+            norm = np.sqrt(dx**2 + dy**2)
+            dx = dx/norm if not np.isclose(norm, 0) else 0
+            dy = dy/norm if not np.isclose(norm, 0) else 0
+            ax.arrow(self.first_gps_data[1][1], self.first_gps_data[1][0], dy*0.00001, dx*0.00001, width =0.00001,head_width=0.00005, head_length=0.00002,ec='blue')
+            self.first_gps_data.pop(0)
 
         ax.scatter(gps_data.data[1], gps_data.data[0], c='blue', label='Robot')
 

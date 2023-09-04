5000eddb16b983f0a6ac69c6d7fe6a6b97d5ef41
Eren Ozturk <erenozt2@gmail.com>
2023-09-04 19:36:51 -0400

    Small refactoring

--- odrive_interface/src/ODrive.py
+++ odrive_interface/src/ODrive.py
@@ -1,7 +1,7 @@
 import os, sys
 currentdir = os.path.dirname(os.path.realpath(__file__))
 sys.path.append(currentdir)
-import odrive_utils
+import init_functions
 import odrive
 import rospy
 from drive_control.msg import WheelSpeed
@@ -53,7 +53,7 @@ class Node_ODriveInterface():
 
             # See if there are any errors, try to clear them once
             if drive_lb.axis0.active_errors != 0:
-                print(f"\nError(s) occurred: ", decode_errors(drive_lb.axis0.active_errors))
+                print(f"\nError(s) occurred: ", init_functions.decode_errors(drive_lb.axis0.active_errors))
                 drive_lb.clear_errors()
 
             self.rate.sleep()
@@ -101,7 +101,7 @@ def calibrate_motors(motor_array):
 
             # Record errors and abort calibration if there are errors
             if odrv.axis0.active_errors != 0:
-                print("Error(s) occurred with motor {} during calibration: {}".format(odrv.serial_number, decode_errors(odrv.axis0.active_errors)))
+                print("Error(s) occurred with motor {} during calibration: {}".format(odrv.serial_number, init_functions.decode_errors(odrv.axis0.active_errors)))
                 errors_encountered = True
                 for odrv in motor_array:
                     odrv.axis0.requested_state = AxisState.IDLE
@@ -144,10 +144,10 @@ def calibrate_motors(motor_array):
         if result != ProcedureResult.SUCCESS:
             calibration_failed = True
             if errors != 0:
-                print("Motor error(s) in motor {}: {}".format(odrv.serial_number, decode_errors(errors)))
-            print("Calibration procedure failed in motor {}. Reason: {}".format(odrv.serial_number, procedure_codes[result]))
+                print("Motor error(s) in motor {}: {}".format(odrv.serial_number, init_functions.decode_errors(errors)))
+            print("Calibration procedure failed in motor {}. Reason: {}".format(odrv.serial_number, init_functions.procedure_codes[result]))
             if result == ProcedureResult.DISARMED:
-                print("Motor {} disarmed. Reason: {}".format(odrv.serial_number, decode_errors(odrv.axis0.disarm_reason)))
+                print("Motor {} disarmed. Reason: {}".format(odrv.serial_number, init_functions.decode_errors(odrv.axis0.disarm_reason)))
             for odrv in motor_array:
                 odrv.axis0.requested_state = AxisState.IDLE
             break
@@ -169,7 +169,7 @@ def calibrate_motors(motor_array):
                 close_loop_achieved = False
             if odrv.axis0.active_errors != 0:
                 errors_occurred = True
-                print("Error(s) occurred: ", decode_errors(odrv.axis0.active_errors))
+                print("Error(s) occurred: ", init_functions.decode_errors(odrv.axis0.active_errors))
                 for odrv in motor_array:
                     odrv.axis0.requested_state = AxisState.IDLE
                 break
@@ -179,33 +179,8 @@ def calibrate_motors(motor_array):
         return False
 
     # Everything went well, every motor is now ready for commands.
+    print("Error(s) occurred: ", init_functions.decode_errors(odrv.axis0.active_errors))
     return True
-
-procedure_codes = ["SUCCESS", "BUSY", "CANCELLED", "DISARMED", "NO_RESPONSE", "POLE_PAIR_CPR_MISMATCH",
-                   "PHASE_RESISTANCE_OUT_OF_RANGE", "PHASE_INDUCTANCE_OUT_OF_RANGE", "UNBALANCED_PHASES",
-                   "UNBALANCED_PHASES", "INVALID_MOTOR_TYPE", "ILLEGAL_HALL_STATE", "TIMEOUT", "HOMING_WITHOUT_ENDSTOP",
-                   "INVALID_STATE", "NOT_CALIBRATED", "NOT_CONVERGING"]
-
-error_codes = {1: "INITIALIZING", 2: "SYSTEM_LEVEL", 4: "TIMING_ERROR",
-               8: "MISSING_ESTIMATE", 16: "BAD_CONFIG", 32: "DRV_FAULT",
-               64: "MISSING_INPUT", 256: "DC_BUS_OVER_VOLTAGE",
-               512: "DC_BUS_UNDER_VOLTAGE", 1024: "DC_BUS_OVER_CURRENT",
-               2048: "DC_BUS_OVER_REGEN_CURRENT", 4096: "CURRENT_LIMIT_VIOLATION",
-               8192: "MOTOR_OVER_TEMP", 16384: "INVERTER_OVER_TEMP",
-               32768: "VELOCITY_LIMIT_VIOLATION", 65536: "POSITION_LIMIT_VIOLATION",
-               16777216: "WATCHDOG_TIMER_EXPIRED", 33554432: "ESTOP_REQUESTED",
-               67108864: "SPINOUT_DETECTED", 134217728: "BRAKE_RESISTOR_DISARMED",
-               268435456: "THERMISTOR_DISCONNECTED", 1073741824: "CALIBRATION_ERROR"}
-
-def decode_errors(n):
-    num_bits = n.bit_length()
-    errors_string = ""
-    for i in range(num_bits):
-        mask = 1 << i
-        if n & mask:
-            errors_string += (error_codes[mask] + ", ")
-
-    return errors_string[:-2]
 
 if __name__ == "__main__":
     driver = Node_ODriveInterface()

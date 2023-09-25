35facc7717375f49108bf3605c8dadc4f5d05dbc
root <blacklotus8@ROS.myguest.virtualbox.org>
2023-09-24 21:35:40 -0400

    moving the view into main-frame

--- angular_ui_app/src/app/ros.service.ts
+++ angular_ui_app/src/app/ros.service.ts
@@ -9,7 +9,12 @@ export class RosService {
 
   constructor() {
     this.ros = new ROSLIB.Ros({
+      // url: 'ws://192.168.0.100:9090' //do not activating port forwarding 
+      // user ng serve --host 0.0.0.0
+      // works over lan, must later be tested with an access point without wifi
+      
       url: 'ws://localhost:9090'
+      
     });
 
     this.ros.on('connection', () => {

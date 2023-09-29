a5c007e15e6d21ba7d80de5d74d5008cf0a907cd
Adam <adam.motaouakkil@mail.mcgill.ca>
2023-09-29 14:30:44 -0500

    Planning of odrive setup task.

---
+++ odrive_interface/src/odrive_setup.py
@@ -0,0 +1,16 @@
+###### TODO: Need to find out how to setup all the configs for the odrives. ######
+
+#--------------------------------------------------------------------------------------------#
+#
+# Goal: Make a generic script that is called at the 
+# initialization of a position (arm) or velocity (drive) controller. 
+# Odrive configuration can come from the UI.
+#
+# Tasks: 
+#       1. Read up on how to setup o-drive and control odrive configuration from the script.
+#           a. Controller state (velocity, position, ramp velocity, ...)
+#           b. velocity, position, acceleration, current and other parameter limits.
+#       2. Make a generic configuration script for any kind of odrive use (drive, arm, ...).
+#       3. Integrate configuration into the UI.
+#
+#--------------------------------------------------------------------------------------------#

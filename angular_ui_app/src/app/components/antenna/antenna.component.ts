9c576b47bfada54e06a24ad83b9316d186b8b661
Blacklotus8 <blacklotus8@NewROS.myguest.virtualbox.org>
2024-01-27 10:27:23 -0500

    finalized the antenna component, to perform final test

--- angular_ui_app/src/app/components/antenna/antenna.component.ts
+++ angular_ui_app/src/app/components/antenna/antenna.component.ts
@@ -12,14 +12,15 @@ export class AntennaComponent {
 
   ros: ROSLIB.Ros;
   sub: ROSLIB.Topic; //subscriber
-  sub2: ROSLIB.Topic; //subscriber
+  // sub2: ROSLIB.Topic; //subscriber
   pub: ROSLIB.Topic; //pub 
 
-  payload2: string = "";
+  // payload2: string = "";
   payload: string = "";
-  latitude: string = "";
-  longitude: string = "";
-  antennaInstruction: string;
+  latitude: number = 0;
+  longitude: number = 0;
+  antennaInstruction: ROSLIB.Message; //payload type
+  // antennaInstruction: string;
   
   constructor(private rosService: RosService) {
     this.ros = this.rosService.getRos();
@@ -29,27 +30,31 @@ export class AntennaComponent {
 
     this.pub = new ROSLIB.Topic({
       ros : this.ros,
-      // name : '/antennaData',
-      // messageType : 'std_msgs/Float32MultiArray'
-            name : '/lol',
-      messageType : 'std_msgs/String'
+      name : '/antennaData',
+      messageType : 'std_msgs/Float32MultiArray'
+      //       name : '/lol',
+      // messageType : 'std_msgs/String'
     });
 
     this.sub = new ROSLIB.Topic({
       ros : this.ros,
-      name : '/chatter',
-      messageType : 'std_msgs/String'
-      // name : '/antennaData',
-      // messageType : 'std_msgs/Float32MultiArray'
+      // name : '/chatter',
+      // messageType : 'std_msgs/String'
+      name : '/antennaData',
+      messageType : 'std_msgs/Float32MultiArray'
     });
 
-    this.sub2 = new ROSLIB.Topic({
-      ros : this.ros,
-      name : '/lol',
-      messageType : 'std_msgs/String'
-      // name : '/antennaData',
-      // messageType : 'std_msgs/Float32MultiArray'
+    this.antennaInstruction = new ROSLIB.Message({
+      data : [0, 0]
     });
+
+    // this.sub2 = new ROSLIB.Topic({
+    //   ros : this.ros,
+    //   name : '/lol',
+    //   messageType : 'std_msgs/String'
+    //   // name : '/antennaData',
+    //   // messageType : 'std_msgs/Float32MultiArray'
+    // });
 
     this.listen()
   }
@@ -60,20 +65,22 @@ export class AntennaComponent {
 
   listen() {
     this.sub.subscribe((message:any) => {
-      // console.log('Received message on ' +this.listener.name + ': ' + message.y + message.x);
+      console.log('Received message on ' +this.sub.name + ': ' + message.data);
       this.payload = ('Received message on ' + this.sub.name +"and the message.data"+  message.data);
-      this.publish(this.payload);
-    });
-
-    this.sub2.subscribe((message:any) => {
-      // console.log('Received message on ' +this.listener.name + ': ' + message.y + message.x);
-      this.payload2 = ('Received message on ' + this.sub2.name +"and the message.data"+  message.data);
+      // this.publish(this.payload);
       
     });
+
+    // this.sub2.subscribe((message:any) => {
+    //   // console.log('Received message on ' +this.listener.name + ': ' + message.y + message.x);
+    //   this.payload2 = ('Received message on ' + this.sub2.name +"and the message.data"+  message.data);
+      
+    // });
     }
 
     sendInstruction() {
-      this.antennaInstruction = this.longitude + this.latitude;
+      this.antennaInstruction = JSON.stringify({data: [this.latitude,this.longitude]});
+      this.pub.publish(this.antennaInstruction);
     }
 
 }

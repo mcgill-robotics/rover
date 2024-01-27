20a06a6d4a95320e3702a662beaef028f2f9ff29
Blacklotus89898 <song.yang.chen89898@gmail.com>
2024-01-27 12:10:39 -0500

    fix the config, fully tested

--- angular_ui_app/src/app/components/antenna/antenna.component.ts
+++ angular_ui_app/src/app/components/antenna/antenna.component.ts
@@ -9,18 +9,16 @@ import { RosService } from 'src/app/ros.service';
   styleUrls: ['./antenna.component.scss']
 })
 export class AntennaComponent {
-
+// ROS fields
   ros: ROSLIB.Ros;
   sub: ROSLIB.Topic; //subscriber
-  // sub2: ROSLIB.Topic; //subscriber
   pub: ROSLIB.Topic; //pub 
 
-  // payload2: string = "";
-  payload: string = "";
+  // Page fields
   latitude: number = 0;
   longitude: number = 0;
-  antennaInstruction: ROSLIB.Message; //payload type
-  // antennaInstruction: string;
+  antennaInstruction: ROSLIB.Message; //payload to be sent
+  payload: string = ""; //payload sent validation
   
   constructor(private rosService: RosService) {
     this.ros = this.rosService.getRos();
@@ -32,14 +30,10 @@ export class AntennaComponent {
       ros : this.ros,
       name : '/antennaData',
       messageType : 'std_msgs/Float32MultiArray'
-      //       name : '/lol',
-      // messageType : 'std_msgs/String'
     });
 
     this.sub = new ROSLIB.Topic({
       ros : this.ros,
-      // name : '/chatter',
-      // messageType : 'std_msgs/String'
       name : '/antennaData',
       messageType : 'std_msgs/Float32MultiArray'
     });
@@ -48,14 +42,7 @@ export class AntennaComponent {
       data : [0, 0]
     });
 
-    // this.sub2 = new ROSLIB.Topic({
-    //   ros : this.ros,
-    //   name : '/lol',
-    //   messageType : 'std_msgs/String'
-    //   // name : '/antennaData',
-    //   // messageType : 'std_msgs/Float32MultiArray'
-    // });
-
+    // starts the liistener
     this.listen()
   }
   
@@ -66,20 +53,13 @@ export class AntennaComponent {
   listen() {
     this.sub.subscribe((message:any) => {
       console.log('Received message on ' +this.sub.name + ': ' + message.data);
-      this.payload = ('Received message on ' + this.sub.name +"and the message.data"+  message.data);
-      // this.publish(this.payload);
-      
+      this.payload = ('Received message on ' + this.sub.name +" and the message.data: "+  message.data);
     });
 
-    // this.sub2.subscribe((message:any) => {
-    //   // console.log('Received message on ' +this.listener.name + ': ' + message.y + message.x);
-    //   this.payload2 = ('Received message on ' + this.sub2.name +"and the message.data"+  message.data);
-      
-    // });
     }
 
     sendInstruction() {
-      this.antennaInstruction = JSON.stringify({data: [this.latitude,this.longitude]});
+      this.antennaInstruction = {data: [this.latitude,this.longitude]};
       this.pub.publish(this.antennaInstruction);
     }
 
--- front_test_pkg/scripts/publisher.py
+++ front_test_pkg/scripts/publisher.py
@@ -1,15 +1,16 @@
 
 import rospy
-from std_msgs.msg import String
+from std_msgs.msg import Float32MultiArray
 
 def talker():
-    pub = rospy.Publisher("chatter", String, queue_size=10)
-    rospy.init_node("talker", anonymous=True)
+    pub = rospy.Publisher("test_topic", Float32MultiArray, queue_size=10)
+    rospy.init_node("antennaData", anonymous=True)
     rate = rospy.Rate(10)
     while not rospy.is_shutdown():
         hello_str = "Hellos woolrd %s" %rospy.get_time()
         rospy.loginfo(hello_str)
-        pub.publish(hello_str)
+        # pub.publish(hello_str)
+        pub.publish(Float32MultiArray(data=[1,1]))
         rate.sleep()
 
 

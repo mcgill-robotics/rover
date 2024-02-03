ac4544cc3949533e441944e199c8fff6b1d89082
Blacklotus89898 <blacklotus8@NewROS.myguest.virtualbox.org>
2024-02-02 23:46:42 -0500

    added the functional rover postion fb, accuracy of computation to be tested

--- angular_ui_app/src/app/components/drive/drive.component.ts
+++ angular_ui_app/src/app/components/drive/drive.component.ts
@@ -10,82 +10,85 @@ import { RosService } from 'src/app/ros.service';
 export class DriveComponent {
   // ROS fields
   ros: ROSLIB.Ros;
-  wheels_sub: ROSLIB.Topic; //subscriber
+  wheels_sub: ROSLIB.Topic;
   position_sub: ROSLIB.Topic
 
-  // Page fields
-  wheels : {
-    right_front: Number;
-    left_front: Number;
-    right_back: Number;
-    left_back: Number;
+  // to be deleted after test output
+  msgstr: String;
+  cmpstr: String;
+
+  // wheel fields are ignored for the moment since no corresponding publisher/topic to subscribe to
+  // wheels : {
+  //   right_front: Number;
+  //   left_front: Number;
+  //   right_back: Number;
+  //   left_back: Number;
+  // }
+
+  // Postion fields
+  payload: {
+    orientation: {
+      w: number;
+      x: number;
+      y: number;
+      z: number;
+    },
+    position: {
+      x: number;
+      y: number;
+      z: number;
+    }
   }
 
-  msgstr: String;
-
-  // orientation : {
-  //   w: Number;
-  //   x: Number;
-  //   y: Number;
-  //   z: Number;
-  // }
-
-  // position : {
-  //   x: Number;
-  //   y: Number;
-  //   z: Number;
-  // }
-
-  // latitude: Number = 0;
-  // longitude: Number = 0;
-  wheelSpeed: ROSLIB.Message; //payload to be sent
-  payload: {
-    orientation : {
-      w: Number;
-      x: Number;
-      y: Number;
-      z: Number;
-    },
-    position : {
-      x: Number;
-      y: Number;
-      z: Number;
-    }
+  computed: {
+    roll: Number;
+    pitch: Number;
+    yaw: Number;
   }
 
   constructor(private rosService: RosService) {
     this.ros = this.rosService.getRos();
   }
 
+  // starting hook
   ngOnInit() {
 
-  
+
     this.position_sub = new ROSLIB.Topic({
       ros: this.ros,
-      name: '/position_pose', //may be odrive
+      name: '/position_pose',
       messageType: 'geometry_msgs/Pose'
     });
-    
+
     this.wheels_sub = new ROSLIB.Topic({
       ros: this.ros,
-      name: '/driveFb"', //may be odrive
+      name: '/driveFb"', //may switch to odrive
       messageType: 'std_msgs/Float32MultiArray'
     });
-
-    // this.antennaInstruction = new ROSLIB.Message({
-    //   data : [0, 0]
-    // });
 
     // starts the liistener
     this.wheel_listen();
     this.position_listen();
   }
 
-  // publish(payload: string) {
-  //   this.pub.publish({data: payload});
-  // }
+  // functions
 
-  
+  // angles are in radians
+  euler_from_quaternion(x: number, y: number, z: number, w: number) {
+    let t0 = +2.0 * (w * x + y * z);
+    let t1 = +1.0 - 2.0 * (x * x + y * y);
+    let roll_x = Math.atan2(t0, t1);
+    let t2 = +2.0 * (w * y - z * x);
+    t2 = (t2 > +1.0) ? +1.0 : t2;
+    t2 = (t2 < -1.0) ? -1.0 : t2;
+    let pitch_y = Math.asin(t2);
+    let t3 = +2.0 * (w * z + x * y);
+    let t4 = +1.0 - 2.0 * (y * y + z * z);
+    let yaw_z = Math.atan2(t3, t4);
+
+    return [roll_x, pitch_y, yaw_z];
+  }
+
   wheel_listen() {
     this.wheels_sub.subscribe((message: any) => {
       console.log('Received message on ' + this.wheels_sub.name + ': ' + message.data);
@@ -97,9 +100,12 @@ export class DriveComponent {
     this.position_sub.subscribe((message: any) => {
       this.payload = message;
       this.msgstr = JSON.stringify(message);
-      
-    });
 
+      let [roll, pitch, yaw] = this.euler_from_quaternion(this.payload.orientation.x, this.payload.orientation.y, this.payload.orientation.z, this.payload.orientation.w);
+      this.computed = { roll, pitch, yaw };
+      this.cmpstr = JSON.stringify(this.computed);
+
+    });
   }
 }
 

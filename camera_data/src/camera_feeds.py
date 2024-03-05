27904e1177f0372914613de05420f61a069b4713
Charles Edward Gagnon <charlesedgagnon@gmail.com>
2024-03-05 10:08:34 -0500

    remove comments on self-explanatory sections of code

--- camera_data/src/camera_feeds.py
+++ camera_data/src/camera_feeds.py
@@ -15,7 +15,6 @@ class CameraHandler:
 
     def get_all_feeds(self):
         ret_frame = []
-        # read each capture object in vids and add as a tuple to retAndFrame
         for camera in self.available_cameras:
             newFeed = camera.read()
             ret_frame.append((newFeed[0], newFeed[1]))
@@ -23,13 +22,12 @@ class CameraHandler:
         return ret_frame
 
     def run_feeds(self):
+        print("Press 'q' to exit")
         while True:
-            # display each frame (camera feed) in the list
             for index, (ret, frame) in enumerate(self.get_all_feeds()):
                 if ret:
                     cv2.imshow(f"frame {index}", frame)
 
-            # press 'q' to stop displaying
             if cv2.waitKey(1) & 0xFF == ord('q'):
                 for vid in self.available_cameras:
                     vid.release()

d7eb3287b1399f8145d244bedb64433e94141cbe
mn297 <Peterwayne3293@gmail.com>
2024-03-16 23:38:05 -0400

    gui tester

--- odrive_interface_arm/src/node_arm_viz.py
+++ odrive_interface_arm/src/node_arm_viz.py
@@ -1,7 +1,5 @@
-#!/usr/bin/env python
 from std_msgs.msg import Float32MultiArray
-from PyQt5.QtWidgets import (QWidget, QVBoxLayout, QHBoxLayout,
-                             QSlider, QLabel, QPushButton, QApplication)
+from PyQt5.QtWidgets import QWidget, QVBoxLayout, QHBoxLayout, QSlider, QLabel, QPushButton, QApplication
 from PyQt5.QtCore import Qt
 import rospy
 import os
@@ -15,51 +13,101 @@ class ArmControlGUI(QWidget):
     def __init__(self):
         super(ArmControlGUI, self).__init__()
         self.initUI()
-        self.ros_setup()
+        self.rosSetup()
 
     def initUI(self):
         self.setWindowTitle('Arm Control GUI')
         vbox = QVBoxLayout()
 
-        self.cmd_slider = QSlider(Qt.Horizontal)
-        self.cmd_slider.setMinimum(0)
-        self.cmd_slider.setMaximum(100)
-        self.cmd_slider.valueChanged[int].connect(self.changeValue)
+        # Define ranges for each joint: (min, max, initial)
+        self.jointRanges = {
+            'Elbow': (-30, 30, 0),
+            'Shoulder': (-30, 30, 0),
+            'Waist': (-90, 90, 0)
+        }
 
-        self.cmd_up_btn = QPushButton("Up")
-        self.cmd_up_btn.clicked.connect(lambda: self.adjust_cmd(1))
-        self.cmd_down_btn = QPushButton("Down")
-        self.cmd_down_btn.clicked.connect(lambda: self.adjust_cmd(-1))
+        self.sliders = []
+        self.labels = []
+        self.downButtons = []
+        self.upButtons = []
 
-        self.fb_label = QLabel('Feedback: 0')
-        self.fb_label.setAlignment(Qt.AlignCenter)
+        for joint, (minVal, maxVal, initVal) in self.jointRanges.items():
+            hbox = QHBoxLayout()
 
-        cmd_hbox = QHBoxLayout()
-        cmd_hbox.addWidget(self.cmd_down_btn)
-        cmd_hbox.addWidget(self.cmd_slider)
-        cmd_hbox.addWidget(self.cmd_up_btn)
+            downBtn = QPushButton("-")
+            self.downButtons.append(downBtn)
 
-        vbox.addLayout(cmd_hbox)
-        vbox.addWidget(self.fb_label)
+            slider = QSlider(Qt.Horizontal)
+            slider.setMinimum(minVal)
+            slider.setMaximum(maxVal)
+            slider.setValue(initVal)  # Set the initial value
+            slider.valueChanged[int].connect(
+                lambda value, j=joint: self.onChangeCmd(j))
+            self.sliders.append(slider)
+
+            upBtn = QPushButton("+")
+            self.upButtons.append(upBtn)
+
+            label = QLabel(f'{joint} Cmd: {initVal}')
+            self.labels.append(label)
+
+            downBtn.clicked.connect(
+                lambda checked, j=joint: self.adjustCmd(j, -1))
+            upBtn.clicked.connect(
+                lambda checked, j=joint: self.adjustCmd(j, 1))
+
+            hbox.addWidget(downBtn)
+            hbox.addWidget(slider)
+            hbox.addWidget(upBtn)
+
+            vbox.addLayout(hbox)
+            vbox.addWidget(label)
+
+        self.fbLabel = QLabel('Fb - Elbow: 0, Shoulder: 0, Waist: 0')
+        self.fbLabel.setAlignment(Qt.AlignCenter)
+        vbox.addWidget(self.fbLabel)
 
         self.setLayout(vbox)
 
-    def adjust_cmd(self, delta):
-        self.cmd_slider.setValue(self.cmd_slider.value() + delta)
+    def adjustCmd(self, joint, delta):
+        index = ['Elbow', 'Shoulder', 'Waist'].index(joint)
+        slider = self.sliders[index]
+        newValue = max(self.jointRanges[joint][0], min(
+            self.jointRanges[joint][1], slider.value() + delta))
+        slider.setValue(newValue)
+        self.onChangeCmd(joint)
 
-    def changeValue(self, value):
-        cmd_msg = Float32MultiArray(data=[float(value)])
-        self.cmd_publisher.publish(cmd_msg)
+    def onChangeCmd(self, joint):
+        cmdMsg = Float32MultiArray()
+        cmdMsg.data = [float(slider.value()) for slider in self.sliders]
+        self.brushlessCmdPublisher.publish(cmdMsg)
+        self.brushedCmdPublisher.publish(cmdMsg)
+        # Update the labels to reflect the current values using triple double quotes for multi-line or complex expressions.
+        for i, label in enumerate(self.labels):
+            label.setText(f"""{["Elbow", "Shoulder", "Waist"][i]} Cmd: {
+                          self.sliders[i].value()}""")
 
-    def update_feedback(self, data):
-        self.fb_label.setText('Feedback: {:.2f}'.format(data.data[0]))
+    def onUpdateBrushlessFb(self, data):
+        self.updateFb('Brushless', data)
 
-    def ros_setup(self):
+    def onUpdateBrushedFb(self, data):
+        self.updateFb('Brushed', data)
+
+    def updateFb(self, motorType, data):
+        fbText = f"""Fb {motorType} - Elbow: {data.data[0]:.2f}, Shoulder: {
+            data.data[1]:.2f}, Waist: {data.data[2]:.2f}"""
+        self.fbLabel.setText(fbText)
+
+    def rosSetup(self):
         rospy.init_node('arm_control_gui', anonymous=True)
-        self.cmd_publisher = rospy.Publisher(
+        self.brushlessCmdPublisher = rospy.Publisher(
             '/armBrushlessCmd', Float32MultiArray, queue_size=10)
+        self.brushedCmdPublisher = rospy.Publisher(
+            '/armBrushedCmd', Float32MultiArray, queue_size=10)
         rospy.Subscriber('/armBrushlessFb', Float32MultiArray,
-                         self.update_feedback)
+                         self.onUpdateBrushlessFb)
+        rospy.Subscriber('/armBrushedFb', Float32MultiArray,
+                         self.onUpdateBrushedFb)
 
 
 def main():

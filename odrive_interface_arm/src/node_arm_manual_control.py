4c10f8660a190dda81e48534f56905692a0833e3
Your Name <peterwayne3293@gmail.com>
2024-03-18 19:38:53 -0400

    tester script cleanup

--- odrive_interface_arm/src/node_arm_manual_control.py
+++ odrive_interface_arm/src/node_arm_manual_control.py
@@ -1,5 +1,13 @@
 from std_msgs.msg import Float32MultiArray
-from PyQt5.QtWidgets import QWidget, QVBoxLayout, QHBoxLayout, QSlider, QLabel, QPushButton, QApplication
+from PyQt5.QtWidgets import (
+    QWidget,
+    QVBoxLayout,
+    QHBoxLayout,
+    QSlider,
+    QLabel,
+    QPushButton,
+    QApplication,
+)
 from PyQt5.QtCore import Qt
 import rospy
 import os
@@ -17,14 +25,14 @@ class ArmControlGUI(QWidget):
         self.downButtons = []
         self.upButtons = []
         self.joint_brushless_lst = {
-            'Elbow': (-30, 30, 0),
-            'Shoulder': (-30, 30, 0),
-            'Waist': (-90, 90, 0)
+            "Elbow": (-30, 30, 0),
+            "Shoulder": (-30, 30, 0),
+            "Waist": (-90, 90, 0),
         }
         self.joint_brushed_lst = {
-            'EndEffector': (-400, 400, 0),
-            'WristRoll': (0, 360, 0),
-            'WristPitch': (-30, 30, 0)
+            "EndEffector": (-400, 400, 0),
+            "WristRoll": (0, 360, 0),
+            "WristPitch": (-30, 30, 0),
         }
         super(ArmControlGUI, self).__init__()
         self.initUI()
@@ -34,23 +42,21 @@ class ArmControlGUI(QWidget):
         mainLayout = QVBoxLayout()
 
         brushlessGroupLayout = self.createMotorGroup(
-            "Brushless", self.joint_brushless_lst)
-        self.fbLabelBrushless = QLabel(
-            'Fb Brushless - Elbow: 0, Shoulder: 0, Waist: 0')
+            "Brushless", self.joint_brushless_lst
+        )
+        self.fbLabelBrushless = QLabel("Fb Brushless - Elbow: 0, Shoulder: 0, Waist: 0")
         self.fbLabelBrushless.setAlignment(Qt.AlignCenter)
         brushlessGroupLayout.addWidget(self.fbLabelBrushless)
         mainLayout.addLayout(brushlessGroupLayout)
 
-        brushedGroupLayout = self.createMotorGroup(
-            "Brushed", self.joint_brushed_lst)
-        self.fbLabelBrushed = QLabel(
-            'Fb Brushed - Elbow: 0, Shoulder: 0, Waist: 0')
+        brushedGroupLayout = self.createMotorGroup("Brushed", self.joint_brushed_lst)
+        self.fbLabelBrushed = QLabel("Fb Brushed - Elbow: 0, Shoulder: 0, Waist: 0")
         self.fbLabelBrushed.setAlignment(Qt.AlignCenter)
         brushedGroupLayout.addWidget(self.fbLabelBrushed)
         mainLayout.addLayout(brushedGroupLayout)
 
         # Reset Button
-        self.resetButton = QPushButton('Reset')
+        self.resetButton = QPushButton("Reset")
         self.resetButton.clicked.connect(self.resetSliders)
         mainLayout.addWidget(self.resetButton)
 
@@ -58,7 +64,7 @@ class ArmControlGUI(QWidget):
 
     def createMotorGroup(self, motorType, jointRanges):
         vbox = QVBoxLayout()
-        vbox.addWidget(QLabel(f'{motorType} Motor Control'))
+        vbox.addWidget(QLabel(f"{motorType} Motor Control"))
 
         for joint, (minVal, maxVal, initVal) in jointRanges.items():
             hbox = QHBoxLayout()
@@ -69,21 +75,19 @@ class ArmControlGUI(QWidget):
             slider = QSlider(Qt.Horizontal)
             slider.setMinimum(minVal)
             slider.setMaximum(maxVal)
-            slider.setValue(initVal)  # Set the initial value
-            slider.valueChanged[int].connect(
-                lambda value, j=joint: self.onChangeCmd(j))
+            # Set the initial value
+            slider.setValue(initVal)
+            slider.valueChanged[int].connect(lambda value, j=joint: self.onChangeCmd(j))
             self.sliders[joint] = slider
 
             upBtn = QPushButton("+")
             self.upButtons.append(upBtn)
 
-            label = QLabel(f'{joint} Cmd: {initVal}')
+            label = QLabel(f"{joint} Cmd: {initVal}")
             self.labels.append(label)
 
-            downBtn.clicked.connect(
-                lambda checked, j=joint: self.adjustCmd(j, -1))
-            upBtn.clicked.connect(
-                lambda checked, j=joint: self.adjustCmd(j, 1))
+            downBtn.clicked.connect(lambda checked, j=joint: self.adjustCmd(j, -1))
+            upBtn.clicked.connect(lambda checked, j=joint: self.adjustCmd(j, 1))
 
             hbox.addWidget(downBtn)
             hbox.addWidget(slider)
@@ -93,12 +97,7 @@ class ArmControlGUI(QWidget):
             vbox.addWidget(label)
             vbox.addLayout(hbox)
 
-        # self.fbLabel[0] = QLabel('Fb - Elbow: 0, Shoulder: 0, Waist: 0')
-
-        # self.fbLabelBrushless.setAlignment(Qt.AlignCenter)
-        # vbox.addWidget(self.fbLabelBrushless)
         return vbox
-        # Brushless motor controls and feedback
 
     def resetSliders(self):
         # Reset brushless sliders
@@ -106,16 +105,18 @@ class ArmControlGUI(QWidget):
             self.sliders[joint].setValue(initVal)
             # Optionally, update the label as well
             index = self.labels.index(
-                next(filter(lambda x: x.text().startswith(joint), self.labels)))
-            self.labels[index].setText(f'{joint} Cmd: {initVal}')
+                next(filter(lambda x: x.text().startswith(joint), self.labels))
+            )
+            self.labels[index].setText(f"{joint} Cmd: {initVal}")
 
         # Reset brushed sliders
         for joint, (minVal, maxVal, initVal) in self.joint_brushed_lst.items():
             self.sliders[joint].setValue(initVal)
             # Optionally, update the label as well
             index = self.labels.index(
-                next(filter(lambda x: x.text().startswith(joint), self.labels)))
-            self.labels[index].setText(f'{joint} Cmd: {initVal}')
+                next(filter(lambda x: x.text().startswith(joint), self.labels))
+            )
+            self.labels[index].setText(f"{joint} Cmd: {initVal}")
 
     def adjustCmd(self, joint, delta):
         # Determine the joint's range based on whether it's in the brushless or brushed list.
@@ -131,8 +132,7 @@ class ArmControlGUI(QWidget):
         slider = self.sliders[joint]
 
         # Calculate the new value, ensuring it stays within the specified range.
-        newValue = max(joint_range[0], min(
-            joint_range[1], slider.value() + delta))
+        newValue = max(joint_range[0], min(joint_range[1], slider.value() + delta))
 
         # Set the new value to the slider.
         slider.setValue(newValue)
@@ -146,49 +146,46 @@ class ArmControlGUI(QWidget):
 
         # For brushless motors
         cmdMsgBrushless = Float32MultiArray()
-        cmdMsgBrushless.data = [float(slider.value())
-                                for key, slider in items[0:3]]
+        cmdMsgBrushless.data = [float(slider.value()) for key, slider in items[0:3]]
         self.brushlessCmdPublisher.publish(cmdMsgBrushless)
 
         # For brushed motors
         cmdMsgBrushed = Float32MultiArray()
-        cmdMsgBrushed.data = [float(slider.value())
-                              for key, slider in items[3:6]]
+        cmdMsgBrushed.data = [float(slider.value()) for key, slider in items[3:6]]
         self.brushedCmdPublisher.publish(cmdMsgBrushed)
 
         # Update labels
         for i, (joint, slider) in enumerate(items):
-            self.labels[i].setText(f'{joint} Cmd: {slider.value()}')
+            self.labels[i].setText(f"{joint} Cmd: {slider.value()}")
 
     def onUpdateBrushlessFb(self, data):
-        self.updateFb('Brushless', data)
+        self.updateFb("Brushless", data)
 
     def onUpdateBrushedFb(self, data):
-        self.updateFb('Brushed', data)
+        self.updateFb("Brushed", data)
 
     def updateFb(self, motorType, data):
-        if motorType == 'Brushless':
+        if motorType == "Brushless":
             names = list(self.joint_brushless_lst.keys())
             fbText = f"""Fb {motorType} - {names[0]}: {data.data[0]:.2f}, {names[1]}: {
                 data.data[1]:.2f}, {names[2]}: {data.data[2]:.2f}"""
             self.fbLabelBrushless.setText(fbText)
-        else:  # Assuming the else block is for 'Brushed'
+        elif motorType == "Brushed":
             names = list(self.joint_brushed_lst.keys())
             fbText = f"""Fb {motorType} - {names[0]}: {data.data[0]:.2f}, {names[1]}: {
                 data.data[1]:.2f}, {names[2]}: {data.data[2]:.2f}"""
-            self.fbLabelBrushed.setText(
-                fbText)  # Corrected to update the brushed label
+            self.fbLabelBrushed.setText(fbText)  # Corrected to update the brushed label
 
     def rosSetup(self):
-        rospy.init_node('arm_control_gui', anonymous=True)
+        rospy.init_node("arm_control_gui", anonymous=True)
         self.brushlessCmdPublisher = rospy.Publisher(
-            '/armBrushlessCmd', Float32MultiArray, queue_size=10)
+            "/armBrushlessCmd", Float32MultiArray, queue_size=10
+        )
         self.brushedCmdPublisher = rospy.Publisher(
-            '/armBrushedCmd', Float32MultiArray, queue_size=10)
-        rospy.Subscriber('/armBrushlessFb', Float32MultiArray,
-                         self.onUpdateBrushlessFb)
-        rospy.Subscriber('/armBrushedFb', Float32MultiArray,
-                         self.onUpdateBrushedFb)
+            "/armBrushedCmd", Float32MultiArray, queue_size=10
+        )
+        rospy.Subscriber("/armBrushlessFb", Float32MultiArray, self.onUpdateBrushlessFb)
+        rospy.Subscriber("/armBrushedFb", Float32MultiArray, self.onUpdateBrushedFb)
 
 
 def main():
@@ -198,5 +195,5 @@ def main():
     sys.exit(app.exec_())
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()

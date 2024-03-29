fc3fc23446d366f0a89d9096a3becfbae6f3d40b
Blacklotus8 <blacklotus8@NewROS.myguest.virtualbox.org>
2024-03-28 22:58:27 -0400

    adding component and base html

---
+++ angular_ui_app/src/app/components/webrtc/webrtc.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-webrtc',
+  templateUrl: './webrtc.component.html',
+  styleUrls: ['./webrtc.component.scss']
+})
+export class WebrtcComponent {
+
+}

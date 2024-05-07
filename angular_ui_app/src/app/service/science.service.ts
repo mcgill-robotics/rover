992389e3cad3b8f3546b408ae17ac460c8a82a1b
Blacklotus89898 <song.yang.chen89898@gmail.com>
2024-05-06 22:37:49 -0400

    data persistence with download, science control

--- angular_ui_app/src/app/service/science.service.ts
+++ angular_ui_app/src/app/service/science.service.ts
@@ -5,13 +5,22 @@ import { Injectable } from '@angular/core';
 })
 export class ScienceService {
   data: any[] = [];
+  data1: number[][][] = [[[0],[0],[0]],[[0],[0],[0]],[[0],[0],[0]],[[0],[0],[0]]]; //4 cuvettes, 3 types(ph, humidity, geiger), arrays for each
   constructor() { }
 
   addData(input: any) {
     this.data.push(input);
   }
 
+  storeData(data:number, cuvetteId:number, dataId:number) {
+    this.data1[cuvetteId][dataId].push(data);
+  }
+
   getData() {
     return this.data;
+  }
+
+  getStoredData() {
+    return this.data1;
   }
 }

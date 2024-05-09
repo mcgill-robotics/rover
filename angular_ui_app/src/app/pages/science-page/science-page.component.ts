6effc76aa6df6e6551f8cd5af5b368aff8f77e0b
Blacklotus89898 <song.yang.chen89898@gmail.com>
2024-05-08 22:49:57 -0400

    all functionality with updated topics

--- angular_ui_app/src/app/pages/science-page/science-page.component.ts
+++ angular_ui_app/src/app/pages/science-page/science-page.component.ts
@@ -11,16 +11,18 @@ import { ScienceService } from 'src/app/service/science.service';
 })
 
 export class SciencePageComponent implements OnInit{
-  data: string;
+  // data: string;
   augerTopic: ROSLIB.Topic;
   carouselTopic: ROSLIB.Topic;
   ros: ROSLIB.Ros;
 
-  // mock prop data
+  // mock topics from electrical
   dataTopic: ROSLIB.Topic;
+  geigerTopic: ROSLIB.Topic;
 
   orientationCounter: number = 0;
 
+  // data array field in front end
   // g1, g2, g3, g4, m1, m, m, etc
   d: number[][] = [[0], [0], [0], [0], [0], [0], [0], [0], [0], [0], [0], [0]];
   
@@ -49,29 +51,40 @@ export class SciencePageComponent implements OnInit{
       messageType : 'std_msgs/Float32MultiArray'
     });
 
+    this.geigerTopic = new ROSLIB.Topic({
+      ros : this.ros,
+      name : "/test_topic2", //to be changed to proper topic name
+      messageType : 'std_msgs/Float32MultiArray'
+    });
+
     this.listen();
   }
 
   listen() {
     this.dataTopic.subscribe((message:any) => {
-      console.log(message.data);  
-      for (let k = 0; k < message.data.length; k++) {
-        if (k < 4 && (k*2 != this.orientationCounter)) { //must be diagonal to update geiger
-          continue //skips a geiger
+      for (let k = 4; k < message.data.length + 4; k++) {
+        // using concat to create new array for ngOnchange to work in graph
+        this.d[k] = this.d[k].concat(message.data[k-4]);
+        // Saving data to store
+        this.scienceService.storeData(message.data[k-4], k);
+      }  
+    })
+
+    this.geigerTopic.subscribe((message:any) => {
+      for (let k = 0; k < 4; k++) {
+        if ((k*2 != this.orientationCounter)) { //must be diagonal to update geiger
+          continue //skips other geiger
         }
         this.d[k] = this.d[k].concat(message.data[k]);
-        // console.log(this.d);
         this.scienceService.storeData(message.data[k], k);
+      }  
+    })    
 
-      }
-        // this.inject = this.inject.concat(message.data[0] as number); //must never pass by ref 
-        
-    })
+
   }
 
-  // Control @@@@@@@@@@@@@@@@@@@
-
-  // carousel
+  
+  // Carousel control
   turn() {
     this.orientationCounter != 7 ? this.orientationCounter++: this.orientationCounter = 0;
 
@@ -80,58 +93,13 @@ export class SciencePageComponent implements OnInit{
     }))
   }
 
-  // auger control //may change depending on electrical implementation
-  sendAugerInstruction(direction: any[]) {
-    console.log(direction)
+  // auger control 
+  //may change depending on electrical implementation
+  sendAugerInstruction(direction: number[]) {
+    // console.log(direction);
     this.augerTopic.publish(new ROSLIB.Message({
-      data : direction //actual data to be added
+      data : direction
     }))
   }
 
-  // up() {
-  //   this.augerTopic.publish(new ROSLIB.Message({
-  //     data : [1, 0] //actual data to be added
-  //   }))
-  // }
-
-  // down() {
-  //   this.augerTopic.publish(new ROSLIB.Message({
-  //     data : [1, 0] //actual data to be added
-  //   }))
-  // }
-  
-  // left() {
-  //   this.augerTopic.publish(new ROSLIB.Message({
-  //     data : [1, 0] //actual data to be added
-  //   }))
-  // }
-  
-  // right () {
-  //   this.augerTopic.publish(new ROSLIB.Message({
-  //     data : [1, 0] //actual data to be added
-  //   }))
-  // }
-
-  // start () {
-  //   this.augerTopic.publish(new ROSLIB.Message({
-  //     data : [1, 0] //actual data to be added
-  //   }))
-  // }
-
-  // stop () {
-  //   this.augerTopic.publish(new ROSLIB.Message({
-  //     data : [1, 0] //actual data to be added
-  //   }))
-  // }
-
-
-  // @@@@@@@@@@ Data Persistence 
-  collect() {
-    this.data = this.scienceService.getData().toString();
-  }
-
-  add() {
-    this.scienceService.addData("halo");
-    this.scienceService.storeData(123,   0);
-  }
 }
